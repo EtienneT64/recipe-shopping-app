e3f4fea603fd2f73b2bb549675c0585c26690321
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2024-01-30 11:35:45 +0200

    working auto login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -29,6 +29,7 @@ export default class AuthComponent implements OnInit, OnDestroy {
   alertHost: PlaceholderDirective;
 
   private closeSub: Subscription;
+  private storeSub: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -38,7 +39,7 @@ export default class AuthComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.store.select('auth').subscribe((authState) => {
+    this.storeSub = this.store.select('auth').subscribe((authState) => {
       this.isLoading = authState.loading;
       this.error = authState.authError;
       if (this.error) {
@@ -71,12 +72,15 @@ export default class AuthComponent implements OnInit, OnDestroy {
   }
 
   onHandleError() {
-    this.error = null;
+    this.store.dispatch(new AuthActions.ClearError());
   }
 
   ngOnDestroy(): void {
     if (this.closeSub) {
       this.closeSub.unsubscribe();
+    }
+    if (this.storeSub) {
+      this.storeSub.unsubscribe();
     }
   }
 
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -36,6 +36,7 @@ export function authReducer(
       return { ...state, user: null };
 
     case AuthActions.LOGIN_START:
+    case AuthActions.SIGNUP_START:
       return {
         ...state,
         authError: null,
@@ -49,6 +50,13 @@ export function authReducer(
         authError: action.payload,
         loading: false,
       };
+
+    case AuthActions.CLEAR_ERROR:
+      return {
+        ...state,
+        authError: null,
+      };
+
     default:
       return state;
   }
